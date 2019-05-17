314986809e2672ef1278e11306ca29ad77c5c002
zemoreira <jose.moreira@ee.ufcg.edu.br>
2019-05-17 00:56:59 -0300

    modifications in the post method to  create a new list

--- routers/kanban-board.js
+++ routers/kanban-board.js
@@ -1,10 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var bodyParser = require('body-parser');
-
+var bodyParser = require("body-parser");
 
 router.use(bodyParser.json()); // for parsing application/jsonng
-
 
 // middleware that is specific to this router
 // router.use(function timeLog (req, res, next) {
@@ -12,101 +10,85 @@ router.use(bodyParser.json()); // for parsing application/jsonng
 //   next();
 // });
 
-
 // Board List  routers
-router.get('/kbboard/list', function (req, res) {
-  console.log('get all board list');
+router.get("/kbboard/list", function(req, res) {
+  console.log("get all board list");
   //database
-  const MongoClient = require('mongodb').MongoClient;
-  const uri = "mongodb+srv://zeigor:Pidpy2018,,minitodo@minitodo-y0izx.mongodb.net/test?retryWrites=true";
+  const MongoClient = require("mongodb").MongoClient;
+  const uri =
+    "mongodb+srv://zeigor:Pidpy2018,,minitodo@minitodo-y0izx.mongodb.net/test?retryWrites=true";
   const client = new MongoClient(uri, { useNewUrlParser: true });
   client.connect(err => {
-    if(!err){
+    if (!err) {
       const db = client.db("minitodo");
       const collection = db.collection("kanbanboard");
       console.log("Connected successfully to server!");
       // get all list of the board
       collection.find().toArray(function(err, docs) {
-        if(err) {
-          res.json('error');
+        if (err) {
+          res.json("error");
         } else {
           res.send(docs);
         }
       });
     } else {
-      res.json('error in server');
+      res.json("error in server");
     }
     client.close();
   });
-    
 });
 
+router.post("/kbboard/list", function(req, res) {
+  console.log("/kbboard/list");
+  console.log("List Name:" + req.body.listName);
 
-router.post('/kbboard/list', function (req, res) {
-  console.log('/kbboard/list');
-  console.log('List Name:' + req.body.listName);
-  
   //database
-  const MongoClient = require('mongodb').MongoClient;
-  const uri = "mongodb+srv://zeigor:Pidpy2018,,minitodo@minitodo-y0izx.mongodb.net/test?retryWrites=true";
+  const MongoClient = require("mongodb").MongoClient;
+  const uri =
+    "mongodb+srv://zeigor:Pidpy2018,,minitodo@minitodo-y0izx.mongodb.net/test?retryWrites=true";
   const client = new MongoClient(uri, { useNewUrlParser: true });
   // connect to database
   client.connect(err => {
-    if(!err){
+    if (!err) {
       const db = client.db("minitodo");
       const collection = db.collection("kanbanboard");
-    
-      // create a list in the board
-      collection.insertOne({name:req.body.listName}, function(err, r) {
-        if(err){
-          res.json({error:'error to create a new list'});
-        } else{
-          //return the created list
-          res.send(r.ops);
-        }
-      });
 
+      collection
+        .find()
+        .sort({ position: -1 })
+        .limit(1)
+        .toArray(function(err, doc) {
+          if (err) {
+            client.close();
+            res.statusCode = 500;
+            res.json({ error: "error to find position column" });
+          } else {
+            console.log(doc);
+            let position = 0;
+            if (doc.length > 0) position = doc[0].position + 1;
+            // create a list in the board
+            collection.insertOne(
+              { name: req.body.listName, position: position },
+              function(err, r) {
+                if (err) {
+                  client.close();
+                  console.log(doc[0].position);
+                  console.log(err);
+                  res.statusCode = 500;
+                  res.json({ error: "error to create a new list" });
+                } else {
+                  res.json(r.ops[0]);
+                  client.close();
+                }
+              }
+            );
+          }
+        });
     } else {
-      res.json({error:'error to create a new list'});
+      res.statusCode = 500;
+      res.json({ error: "error connect to database" });
     }
-    client.close();
   });
 });
 
-
 module.exports = router;
-
-
-// client.connect(err => {
-//   const collection = client.db("minitodo").collection("kanbanboard");
-//   console.log("Connected successfully to server!");
-//   // Insert a single document
-//   board = {name:'board',
-//           labels: [
-//             {
-//               name:'',
-//               color:'red',
-//               class:'label__color--watermellon',
-//             },
-//             {
-//               name:'',
-//               color:'blue',
-//               class:'label__color--sky',
-//             },
-//           ],
-//            lists: [
-//              {
-//                name: 'list 1'
-//              },
-//              {
-//               name: 'list 2'
-//             }
-
-//            ]};
-//   collection.insertOne(board, function(err, r) {
-
-//   });
-
-
-//   client.close();
-// });
